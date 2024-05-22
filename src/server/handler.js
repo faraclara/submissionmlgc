95fd4607832db574e7e34b8e8376bb3c0d0680bc
faraclara <faraadevianaaa@gmail.com>
2024-05-23 00:07:41 +0700

    secong commit

--- src/server/handler.js
+++ src/server/handler.js
@@ -22,7 +22,7 @@ async function postPredictHandler(request, h) {
 
   const response = h.response({
     status: 'success',
-    message: confidenceScore > 99 ? 'Model is predicted successfully' : 'Model is predicted successfully but under threshold. Please use the correct picture',
+    message: confidenceScore >= 100 || confidenceScore < 1 ? 'Model is predicted successfully' : 'Model is predicted successfully but under threshold. Please use the correct picture',
     data
   })
   response.code(201);
