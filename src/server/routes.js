1c8c0e44b731bdaedf695da534c5f4b1812f4712
faraclara <faraadevianaaa@gmail.com>
2024-05-22 14:36:40 +0700

    first commit

---
+++ src/server/routes.js
@@ -0,0 +1,25 @@
+const postPredictHandler = require('../server/handler');
+const getPredictionHistoriesHandler = require('../server/historyHandler');
+
+const routes = [
+    {
+        path: '/predict',
+        method: 'POST',
+        handler: postPredictHandler,
+        options: {
+            payload: {
+                allow: 'multipart/form-data',
+                multipart: true
+            }
+        }
+    },
+    {
+        path: '/predict/histories',
+        method: 'GET',
+        handler: getPredictionHistoriesHandler
+    }
+]
+
+module.exports = routes;
+
+module.exports = routes;
